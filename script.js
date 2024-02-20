8734360fbec620240044735a506b794881bf19fb
Senay1675 <senay_93@hotmail.com>
2024-02-20 11:32:43 +0100

    added files

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputHabit = document.querySelector("#habitInput");
+const lowBtn = document.querySelector("#low");
+const mediumBtn = document.querySelector("#medium");
+const highBtn = document.querySelector("#high");
+const addHabit = document.querySelector("addHabitBtn");
+
+addHabit.addEventListener("click", ()=>{
+    
+})
