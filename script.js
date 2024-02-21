8c11a0c681d85bc152f0461b01469c8b8685c19e
Jon Merilä <jon.merila97@hotmail.com>
2024-02-21 12:57:23 +0100

    Added sorting for Time est

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const todoTitle = document.querySelector("#todoName");
 const addTodoBtn = document.querySelector("#addTodo");
+
 const todoDesc = document.querySelector("#todoDesc");
 const todoDate = document.querySelector("#todoDate");
+
 const todoEstValue = document.querySelector("#estTime");
 const todoEst = document.querySelector("#todoEstimate");
+
 const todoCategory = document.querySelector("#todoCategory");
 const todoList = document.querySelector(".your-todo");
+
+const sortFilter = document.querySelector("#riseFall");
+const deadlineCheck = document.querySelector("#deadlineEst");
+const timeCheck = document.querySelector("#timeEst");
+console.log(deadlineCheck.checked, timeCheck.checked);
 
 const createTodoItem = (
   title,
@@ -28,8 +36,8 @@ const createTodoItem = (
   let status = createDiv("todoStatus");
 
   status.innerHTML = `<span>To-do</span>
-  <span>Est. time: ${estimation} ${estimationUnit}</span>
-  <span>Deadline: ${deadline}</span>`;
+  <span>Est. time: <span>${estimation}</span> ${estimationUnit}</span>
+  <span>Deadline: <span>${deadline}</span></span>`;
 
   //Append to mid card position
   let todoDetails = createDiv("todoDetails");
@@ -46,7 +54,93 @@ const createTodoItem = (
   middleTodo.append(todoDetails);
   lowerTodo.append(doneBtn, editBtn, deleteBtn);
   todoCard.append(upperTodo, middleTodo, lowerTodo);
+
   return todoCard;
+};
+
+sortFilter.addEventListener("change", () => {
+  let filter = sortFilter.value;
+  timeSort(filter);
+});
+
+const deadlineSort = (filter) => {
+  console.log(filter);
+  const todoCards = Array.from(todoList.getElementsByClassName("todo"));
+
+  todoCards.sort((a, b) => {
+    const timeA = extractTimeDate(a);
+    const timeB = extractTimeDate(b);
+    console.log(timeA);
+    if (filter === "rising") {
+      return timeA - timeB;
+    } else if (filter === "falling") {
+      return timeB - timeA;
+    }
+  });
+
+  todoList.innerHTML = "";
+  todoCards.forEach((card) => {
+    todoList.appendChild(card);
+  });
+};
+
+const timeSort = (filter) => {
+  console.log(filter);
+  const todoCards = Array.from(todoList.getElementsByClassName("todo"));
+
+  todoCards.sort((a, b) => {
+    const timeA = extractTimeDate(a);
+    const timeB = extractTimeDate(b);
+    console.log(timeA);
+    if (filter === "rising") {
+      return timeA - timeB;
+    } else if (filter === "falling") {
+      return timeB - timeA;
+    }
+  });
+
+  todoList.innerHTML = "";
+  todoCards.forEach((card) => {
+    todoList.appendChild(card);
+  });
+};
+
+const extractTimeDate = (todoCard) => {
+  if (timeCheck.checked) {
+    const details = todoCard.querySelector(
+      ".todoStatus span:nth-child(2) > span"
+    );
+    console.log(details);
+    if (!details) return 0;
+    console.log(details);
+    const estText = details.textContent;
+    console.log(estText);
+    const [estimation, _] = estText.split(" ");
+    return parseInt(estimation) || 0;
+  }
+  if (deadlineCheck.checked) {
+    const details = todoCard.querySelector(
+      ".todoStatus span:nth-child(3) > span"
+    );
+    console.log(details);
+    if (!details) return 0;
+
+    const estText = details.textContent;
+    console.log(estText);
+
+    const deadlineDate = new Date(estText);
+    console.log(deadlineDate);
+
+    if (isNaN(deadlineDate.getTime())) {
+      console.error("Invalid date format:", deadline);
+      return null;
+    }
+    return deadlineDate;
+  }
+};
+
+const extractTodoDate = (todoCard) => {
+  const details = todoCard.querySelector("");
 };
 
 const createDiv = (className) => {
@@ -77,9 +171,10 @@ todoDate.addEventListener("change", () => {
 addTodoBtn.addEventListener("click", () => {
   const title = todoTitle.value;
   const description = todoDesc.value;
-  const estimation = todoEstValue.value;
+  const estimation = todoEst.value;
   const estimationUnit = todoEstValue.value;
   const deadline = todoDate.value;
+  console.log(estimation);
 
   const todoItem = createTodoItem(
     title,
