b1f1d64cf6191b3de6db8115f3d3f95a9aa44bfe
Senay1675 <senay_93@hotmail.com>
2024-02-21 13:18:44 +0100

    gjort klart counter

--- script.js
+++ script.js
@@ -94,12 +94,12 @@ addTodoBtn.addEventListener("click", () => {
 /* ----------- LOGIN FUNKTIONALITET -------------- */
 
 
-const loginUsernameInput = document.querySelector("#loginUsername");
-const loginPasswordInput = document.querySelector("#loginPassword");
-const loginRegisterBtn = document.querySelector(".loginRegisterBtn");
-const loginBtn = document.querySelector(".loginBtn");
+// const loginUsernameInput = document.querySelector("#loginUsername");
+// const loginPasswordInput = document.querySelector("#loginPassword");
+// const loginRegisterBtn = document.querySelector(".loginRegisterBtn");
+// const loginBtn = document.querySelector(".loginBtn");
 
-let users = JSON.parse(localStorage.getItem("users")) || [];
+// let users = JSON.parse(localStorage.getItem("users")) || [];
 
 
 // loginRegisterBtn.addEventListener("click", () => {
@@ -107,36 +107,36 @@ let users = JSON.parse(localStorage.getItem("users")) || [];
 //   let password = loginPasswordInput.value;
 
 
-  let newUser = {
-    username,
-    password,
-  };
+//   let newUser = {
+//     username,
+//     password,
+//   };
 
-  users.push(newUser);
+//   users.push(newUser);
 
-  localStorage.setItem("users", JSON.stringify(users));
+//   localStorage.setItem("users", JSON.stringify(users));
 
-  console.log("Användaren har registrerats", newUser);
-  //   localStorage.setItem("name", loginUsernameInput.value);
+//   console.log("Användaren har registrerats", newUser);
+//   //   localStorage.setItem("name", loginUsernameInput.value);
 
 
-loginBtn.addEventListener("click", () => {
-  let username = loginUsernameInput.value;
-  let password = loginPasswordInput.value;
+// loginBtn.addEventListener("click", () => {
+//   let username = loginUsernameInput.value;
+//   let password = loginPasswordInput.value;
 
-  let user = users.find(
-    (user) => user.username === username && user.password === password
-  );
+//   let user = users.find(
+//     (user) => user.username === username && user.password === password
+//   );
 
-  let h2 = document.querySelector("#loginH2");
-  console.log(user);
+//   let h2 = document.querySelector("#loginH2");
+//   console.log(user);
 
-  if (user) {
-    h2.textContent = `Welcome ${user.username}`;
-  } else {
-    h2.textContent = `Failed 2 login`;
-  }
-});
+//   if (user) {
+//     h2.textContent = `Welcome ${user.username}`;
+//   } else {
+//     h2.textContent = `Failed 2 login`;
+//   }
+// });
 
 /* ----------- LOGIN FUNKTIONALITET SLUT ---------- */
 
@@ -148,17 +148,65 @@ const mediumBtn = document.querySelector("#medium");
 const highBtn = document.querySelector("#high");
 const addHabit = document.querySelector("#addHabitBtn");
 const habitCardContainer = document.querySelector("#habitCard-container");
+console.log(habitCardContainer);
+
+// När man trycker på knappen så dyker kortet för habits fram
 
 addHabit.addEventListener("click", () => {
   let habitCard = document.createElement("div");
   let habitName = document.createElement("h4");
   habitName.innerText = inputHabit.value;
 
-  habitCard.append(habitName);
+  habitCard.append(habitName)
 
-  // Lägg till habitCard i DOM-trädet, till exempel genom att lägga till det till body
-  document.body.appendChild(habitCard);
-  //habitCard.append(habitName);
+  habitCardContainer.append(habitCard);
+
+  let habitCounter = document.createElement("div");
+  let habitStreakTitle = document.createElement("h3")
+  habitStreakTitle.innerText = "Streak";
+
+habitCounter.append(habitStreakTitle);
+habitCard.append(habitCounter);
+
+let counterDiv = document.createElement("div")
+
+let incrementBtn = document.createElement("button");
+incrementBtn.innerText = "+";
+let decrementBtn = document.createElement("button");
+decrementBtn.innerText = "-";
+let resetBtn = document.createElement("button"); 
+resetBtn.innerText = "Reset";
+let counterValue = document.createElement("div")
+counterValue.innerText = "0";
+
+
+
+counterDiv.appendChild(incrementBtn);
+counterDiv.appendChild(counterValue);
+counterDiv.appendChild(decrementBtn);
+counterDiv.appendChild(resetBtn);
+habitCard.appendChild(counterDiv);
+
+let counter = 0;
+
+incrementBtn.addEventListener("click",() =>{
+    counter++;
+    counterValue.innerText = counter;
+});
+
+decrementBtn.addEventListener("click", () =>{
+    counter--;
+    counterValue.innerText = counter;
+
+});
+
+resetBtn.addEventListener("click", () =>{
+    counter = 0;
+    counterValue.innerText = counter;
+})
+
+
+
 });
 
 /* ------------------- Habits ---------------------- */
