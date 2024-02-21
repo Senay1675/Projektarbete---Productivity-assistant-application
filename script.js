d5a94e9fa1998b4ee01e72afd042ba3c785cfcdf
CharlesLindberg <charleslindberg91@gmail.com>
2024-02-21 13:15:00 +0100

    kommenterat in

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-// const todoTitle = document.querySelector("#todoName");
-// const addTodoBtn = document.querySelector("#addTodo");
-// const todoDesc = document.querySelector("#todoDesc");
-// const todoDate = document.querySelector("#todoDate");
-// const todoEstValue = document.querySelector("#estTime");
-// const todoEst = document.querySelector("#todoEstimate");
-// const todoCategory = document.querySelector("#todoCategory");
-// const todoList = document.querySelector(".your-todo");
+const todoTitle = document.querySelector("#todoName");
+const addTodoBtn = document.querySelector("#addTodo");
+const todoDesc = document.querySelector("#todoDesc");
+const todoDate = document.querySelector("#todoDate");
+const todoEstValue = document.querySelector("#estTime");
+const todoEst = document.querySelector("#todoEstimate");
+const todoCategory = document.querySelector("#todoCategory");
+const todoList = document.querySelector(".your-todo");
 
-// addTodoBtn.addEventListener("click", () => {
-//   let todoCard = document.createElement("div");
-//   todoCard.classList.add("todo");
-//   let status = document.createElement("div");
-//   status.classList.add("todoStatus");
-//   let statusP = document.createElement("span");
-//   statusP.textContent = "To-do";
-//   let todoName = document.createElement("h4");
-//   todoName.textContent = todoTitle.value;
-//   let description = document.createElement("p");
-//   description.textContent = todoDesc.value;
-//   let estimate = document.createElement("span");
-//   estimate.innerHTML = `<p>Est.time: ${todoEst.value} ${todoEstValue.value}</p>`;
+addTodoBtn.addEventListener("click", () => {
+  let todoCard = document.createElement("div");
+  todoCard.classList.add("todo");
+  let status = document.createElement("div");
+  status.classList.add("todoStatus");
+  let statusP = document.createElement("span");
+  statusP.textContent = "To-do";
+  let todoName = document.createElement("h4");
+  todoName.textContent = todoTitle.value;
+  let description = document.createElement("p");
+  description.textContent = todoDesc.value;
+  let estimate = document.createElement("span");
+  estimate.innerHTML = `<p>Est.time: ${todoEst.value} ${todoEstValue.value}</p>`;
 
-//   todoCard.append(estimate);
-//   status.append(statusP);
-//   todoCard.append(status);
-//   todoCard.append(todoName);
-//   todoCard.append(description);
-//   todoList.append(todoCard);
-//   console.log(todoCard);
-// });
+  todoCard.append(estimate);
+  status.append(statusP);
+  todoCard.append(status);
+  todoCard.append(todoName);
+  todoCard.append(description);
+  todoList.append(todoCard);
+  console.log(todoCard);
+});
 // ...
 /* ----------- LOGIN FUNKTIONALITET -------------- */
 
@@ -83,18 +83,18 @@ loginBtn.addEventListener("click", () => {
 
 /* ------------------- Habits ---------------------- */
 
-// const inputHabit = document.querySelector("#habitInput");
-// const lowBtn = document.querySelector("#low");
-// const mediumBtn = document.querySelector("#medium");
-// const highBtn = document.querySelector("#high");
-// const addHabit = document.querySelector("#addHabitBtn");
-// const habitCardContainer = document.querySelector("#habitCard-container")
+const inputHabit = document.querySelector("#habitInput");
+const lowBtn = document.querySelector("#low");
+const mediumBtn = document.querySelector("#medium");
+const highBtn = document.querySelector("#high");
+const addHabit = document.querySelector("#addHabitBtn");
+const habitCardContainer = document.querySelector("#habitCard-container")
 
-// addHabit.addEventListener("click", () => {
-//     let habitCard = document.createElement("div");
-//     let habitName = document.createElement("h4");
-//     habitName.innerText = inputHabit.value;
+addHabit.addEventListener("click", () => {
+    let habitCard = document.createElement("div");
+    let habitName = document.createElement("h4");
+    habitName.innerText = inputHabit.value;
 
-// addHabit.addEventListener("click", ()=>{
+addHabit.addEventListener("click", ()=>{
 
-// })
+})
